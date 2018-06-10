6afb2b8048e040c7a9879244165133c4eea02e3a
Rafael PacMe <rafy.pachecomendez@gmail.com>
2018-06-09 21:30:59 -0400

    rpi-common: fix architecture to arm

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,10 +23,10 @@ TARGET_NO_RECOVERY := true
 TARGET_BOARD_PLATFORM := bcm2710
 
 # Architecture
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
 # Kernel
