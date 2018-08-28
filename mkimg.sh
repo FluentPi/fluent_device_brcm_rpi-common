83380d138959d04c6431d8c98ec07c6799e138d0
Rafael Pacheco Méndez <rafy.pachecomendez@gmail.com>
2018-08-28 00:02:57 -0300

    mkimg: remove devmappings if finding loop device fails
    Change-Id: I9cf83c751d7e98a6c6cdf28fcb5983eb62a1e7c8

--- mkimg.sh
+++ mkimg.sh
@@ -14,31 +14,28 @@ if [ -f $IMGNAME ]; then
 	echo "File $IMGNAME already exists!"
 else
 	echo "Creating image file $IMGNAME..."
-	dd if=/dev/zero of=$IMGNAME bs=512k count=8142
+	dd if=/dev/zero of=$IMGNAME bs=512k count=$(echo "$IMGSIZE*1024*2" | bc)
 	sync
 	echo "Creating partitions..."
-	kpartx -a $IMGNAME
-	sync
 	(
 	echo o
 	echo n
 	echo p
 	echo 1
 	echo
-	echo +128
+	echo +128M
 	echo n
 	echo p
 	echo 2
 	echo
-	echo +1024
+	echo +1024M
 	echo n
 	echo p
 	echo 3
 	echo
-	echo +256
+	echo +256M
 	echo n
 	echo p
-	echo 4
 	echo
 	echo
 	echo t
@@ -47,32 +44,36 @@ else
 	echo a
 	echo 1
 	echo w
-	) | fdisk /dev/loop0
+	) | fdisk $IMGNAME
 	sync
-	kpartx -d $IMGNAME
+	LOOPDEV=`kpartx -av $IMGNAME | awk 'NR==1{ sub(/p[0-9]$/, "", $3); print $3 }'`
 	sync
-	kpartx -a $IMGNAME
-	sync
+	if [ -z "$LOOPDEV" ]; then
+		echo "Unable to find loop device!"
+		kpartx -d $IMGNAME
+		exit
+	fi
+	echo "Image mounted as $LOOPDEV"
 	sleep 5
-	dd if=fat-boot/boot.fat of=/dev/mapper/loop0p1
-	mkfs.ext4 /dev/mapper/loop0p4
-	resize2fs /dev/mapper/loop0p4 687868
+	mkfs.fat -F 32 /dev/mapper/${LOOPDEV}p1
+	mkfs.ext4 /dev/mapper/${LOOPDEV}p4
+	resize2fs /dev/mapper/${LOOPDEV}p4 687868
 	echo "Copying system..."
-	dd if=../../../out/target/product/rpi/system.img of=/dev/mapper/loop0p2 bs=1M
+	dd if=$OUTDIR/system.img of=/dev/mapper/${LOOPDEV}p2 bs=1M
 	echo "Copying vendor..."
-	dd if=../../../out/target/product/rpi/vendor.img of=/dev/mapper/loop0p3 bs=1M
+	dd if=$OUTDIR/vendor.img of=/dev/mapper/${LOOPDEV}p3 bs=1M
 	echo "Copying boot..."
 	mkdir -p sdcard/boot
 	sync
-	mount /dev/mapper/loop0p1 sdcard/boot
+	mount /dev/mapper/${LOOPDEV}p1 sdcard/boot
 	sync
 	cp boot/* sdcard/boot
-	cp ../../../vendor/brcm/rpi/proprietary/boot/* sdcard/boot
-	cp ../../../out/target/product/rpi/obj/KERNEL_OBJ/arch/arm/boot/zImage sdcard/boot
-	cp -R ../../../out/target/product/rpi/obj/KERNEL_OBJ/arch/arm/boot/dts/* sdcard/boot
-	cp ../../../out/target/product/rpi/ramdisk.img sdcard/boot
+	cp ../../../vendor/brcm/rpi3/proprietary/boot/* sdcard/boot
+	cp $OUTDIR/obj/KERNEL_OBJ/arch/arm/boot/zImage sdcard/boot
+	cp -R $OUTDIR/obj/KERNEL_OBJ/arch/arm/boot/dts/* sdcard/boot
+	cp $OUTDIR/ramdisk.img sdcard/boot
 	sync
-	umount /dev/mapper/loop0p1
+	umount /dev/mapper/${LOOPDEV}p1
 	rm -rf sdcard
 	kpartx -d $IMGNAME
 	sync
